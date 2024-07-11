76da60851ab3cb641c145ff7c3af7e8274dbf085
ebriancs <ebrian.cs@gmail.com>
2024-07-11 22:46:54 +0800

    updated

--- ip_capture/models.py
+++ ip_capture/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.utils import timezone
 
 class UserDetail(models.Model):
-    ip_address = models.GenericIPAddressField()
+    ip_address = models.GenericIPAddressField(null=True, blank=True)
     latitude = models.DecimalField(max_digits=15, decimal_places=10, null=True, blank=True)
     longitude = models.DecimalField(max_digits=15, decimal_places=10, null=True, blank=True)
-    device_info = models.JSONField()
+    device_info = models.JSONField(null=True, blank=True)
     timestamp = models.DateTimeField(default=timezone.now)
